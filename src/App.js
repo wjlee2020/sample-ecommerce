452ff1be0cd8b5e6e0e434f039f3873e53871396
WJ Lee <wjlee2122@gmail.com>
2021-01-24 08:42:36 +0900

    set up basic router
    BrowserRouter, basic link / switch / route to Cart.js and Photos.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Header from './components/Header';
+import Photos from './pages/Photos';
+import Cart from './pages/Cart';
 
 function App() {
   return (
     <div>
+      <Header />
+
+      <Switch>
+
+        <Route exact path="/">
+          <Photos/>
+        </Route>
+
+        <Route exact path="/cart">
+          <Cart/>
+        </Route>
+
+      </Switch>
     </div>
   );
 }
