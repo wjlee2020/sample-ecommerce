a328c77d50c60a60ded091b5c93551392fda7da8
Lee <wjlee@tangerine.io>
2021-01-28 15:05:34 +0900

    toggle fav and change heart icon
    set toggleFavorite function, and heart icon when it's favorited

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { Context } from '../Context';
 
 function Image({ className, img }) {
     const [isHovered, setIsHovered] = useState(false)
 
-    const heartIcon = isHovered && <i className="ri-heart-line favorite"></i>;
+    const { toggleFavorite } = useContext(Context)
+
+    function heartIcon() {
+        if (img.isFavorite) {
+            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
+        } else if (isHovered) {
+            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
+        }
+    }
+
     const cartIcon = isHovered && <i className="ri-add-circle-line cart"></i>;
 
     return (
@@ -16,7 +26,7 @@ function Image({ className, img }) {
             }}
             className={`${className} image-container`}>
             <img className='image-grid' src={img.url} alt="stock photos" />
-            {heartIcon}
+            {heartIcon()}
             {cartIcon}
         </div>
     )
