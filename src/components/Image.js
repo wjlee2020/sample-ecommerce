d9162aa0913ee740fd32baff2f1b61a34379252f
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2022-03-06 16:13:20 +0900

    chore: updated packages

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,53 +1,67 @@
-
-import React, { useState, useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Context } from '../Context';
-import HoverHook from '../customHooks/HoverHook';
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import { Context } from "../Context";
+import HoverHook from "../customHooks/HoverHook";
 
 function Image({ className, img }) {
-    // const [hovered, setHovered] = useState(false)
-    const [hovered, ref] = HoverHook();
+	const [hovered, ref] = HoverHook();
 
-    const {
-        toggleFavorite,
-        addCartItem,
-        cartItems,
-        removeCartItem
-    } = useContext(Context)
+	const { toggleFavorite, addCartItem, cartItems, removeCartItem } =
+		useContext(Context);
 
-    function heartIcon() {
-        if (img.isFavorite) {
-            return <i className="ri-heart-fill favorite" onClick={() => toggleFavorite(img.id)}></i>
-        } else if (hovered) {
-            return <i className="ri-heart-line favorite" onClick={() => toggleFavorite(img.id)}></i>
-        }
-    }
+	function heartIcon() {
+		if (img.isFavorite) {
+			return (
+				<i
+					className="ri-heart-fill favorite"
+					onClick={() => toggleFavorite(img.id)}
+				></i>
+			);
+		} else if (hovered) {
+			return (
+				<i
+					className="ri-heart-line favorite"
+					onClick={() => toggleFavorite(img.id)}
+				></i>
+			);
+		}
+	}
 
-    function cartIcon() {
-        const alreadyCarted = cartItems.some(item => item.id === img.id);
-        if (alreadyCarted) {
-            return <i className="ri-shopping-cart-fill cart" onClick={() => removeCartItem(img.id)}></i>
-        } else if (hovered) {
-            return <i className="ri-add-circle-line cart" onClick={() => addCartItem(img)}></i>;
-        }
-    }
+	function cartIcon() {
+		const alreadyCarted = cartItems.some((item) => item.id === img.id);
+		if (alreadyCarted) {
+			return (
+				<i
+					className="ri-shopping-cart-fill cart"
+					onClick={() => removeCartItem(img.id)}
+				></i>
+			);
+		} else if (hovered) {
+			return (
+				<i
+					className="ri-add-circle-line cart"
+					onClick={() => addCartItem(img)}
+				></i>
+			);
+		}
+	}
 
-    return (
-        <div ref={ref} className={`${className} image-container`}>
-            <img className='image-grid' src={img.url} alt="stock photos" />
-            {heartIcon()}
-            {cartIcon()}
-        </div>
-    )
+	return (
+		<div ref={ref} className={`${className} image-container`}>
+			<img className="image-grid" src={img.url} alt="stock photos" />
+			{heartIcon()}
+			{cartIcon()}
+		</div>
+	);
 }
 
 Image.propTypes = {
-    className: PropTypes.string,
-    img: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        url: PropTypes.string.isRequired,
-        isFavorite: PropTypes.bool
-    })
-}
+	className: PropTypes.string,
+	img: PropTypes.shape({
+		id: PropTypes.string.isRequired,
+		url: PropTypes.string.isRequired,
+		isFavorite: PropTypes.bool,
+	}),
+};
 
 export default Image;
